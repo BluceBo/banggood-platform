ce68e04f2b5f444de186024a7a38d395ef06c569
tanbo <blucebo@163.com>
2018-01-23 18:19:57 +0800

    搭建注册中心与管理模块
    搭建注册中心与管理模块

---
+++ manage-service/src/main/java/com/banggood/ManageServiceApplication.java
@@ -0,0 +1,12 @@
+package com.banggood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageServiceApplication.class, args);
+	}
+}
