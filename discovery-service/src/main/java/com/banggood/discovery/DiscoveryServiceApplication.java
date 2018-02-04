2b23b4b9bc1f9388523326627b3a2009bb1d162a
BluceBo <blucebo@163.com>
2018-02-04 21:36:46 +0800

    完善管理服务

--- discovery-service/src/main/java/com/banggood/discovery/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/banggood/discovery/DiscoveryServiceApplication.java
@@ -1,4 +1,4 @@
-package com.banggood;
+package com.banggood.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ manage-web/src/main/java/com/banggood/manage/ManageWebApplication.java
@@ -0,0 +1,12 @@
+package com.banggood.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageWebApplication.class, args);
+	}
+}
